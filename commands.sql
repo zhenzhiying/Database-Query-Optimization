91108c7d38e5dc6211bc337bea867c429e019740
mztse <ziaotse@hotmail.com>
2015-06-17 22:35:48 +0800

    Add second sql

--- commands.sql
+++ commands.sql
@@ -1,3 +1,13 @@
 explain select *
 from basic
 where name='林师傅';
+
+create index index_name on basic(name);
+
+#----------------------------------------------------------
+
+explain select phone
+from basic
+where name='巫山烤全鱼' and avg_price<50;
+
+create index index_name on basic(name);
